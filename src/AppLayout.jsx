df4fd9c8c7f254f3b513407a189656675aae033d
EddieMollerstrom <eddiemollerstrom123@gmail.com>
2024-03-19 15:02:25 +0100

    saved css

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function AppLayout() {
   return (
@@ -8,6 +9,7 @@ export default function AppLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
