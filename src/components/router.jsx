7670fd023f6bec0fedc97eea127ee7b323529abe
jakob-olsson <jakobolsson13@gmail.com>
2024-03-19 14:54:37 +0100

    Added react outlet.

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header.jsx";
 import Home from "../pages/Home.jsx";
 import ChangeMyName from "../pages/ChangeMyName.jsx";
+import AppLayout from "../AppLayout.jsx";
 
 export default function Router() {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ChangeMyName" element={<ChangeMyName />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/ChangeMyName" element={<ChangeMyName />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
