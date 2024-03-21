5a0d6a6665194e900d343eee7d74d107510a433f
jakob-olsson <jakobolsson13@gmail.com>
2024-03-21 15:35:29 +0100

    Created the contact form and the styling for it.

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import ChangeMyName from "../pages/ChangeMyName.jsx";
 import AppLayout from "../AppLayout.jsx";
+import Contact from "../pages/Contact.jsx";
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
           <Route path="/" element={<AppLayout />}>
             <Route index element={<Home />} />
             <Route path="/ChangeMyName" element={<ChangeMyName />} />
+            <Route path="/Contact" element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
