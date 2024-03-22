4a64e421c23d39a91bc97ef90f25ca7282e4943a
Skarader <lukasholmwolf@gmail.com>
2024-03-22 11:58:19 +0100

    fixed tailwind

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -2,22 +2,22 @@ export default function LoginSignup() {
   return (
     <>
       <div className="login-signup-main-container">
-        <div className="login-container">
+        <div className="login-container bg-custom-green">
           <h2 className="title-login">VÄLKOMMEN TILLBAKA</h2>
           <div className="login-position-handler">
             <input type="text" placeholder="ANVÄNDARNAMN" />
             <input type="password" placeholder="LÖSENORD" />
           </div>
-          <button className="login-btn">LOGGA IN</button>
+          <button className="login-btn bg-custom-yellow">LOGGA IN</button>
         </div>
-        <div className="signup-container">
+        <div className="signup-container bg-custom-white">
           <h2 className="title-signup">SKAPA KONTO</h2>
           <div className="signup-position-handler">
             <input type="text" placeholder="ANVÄNDARNAMN" />
             <input type="email" placeholder="EMAIL" />
             <input type="password" placeholder="LÖSENORD" />
           </div>
-          <button className="signup-btn">SIGN UP</button>
+          <button className="signup-btn bg-custom-yellow">SIGN UP</button>
         </div>
       </div>
     </>
