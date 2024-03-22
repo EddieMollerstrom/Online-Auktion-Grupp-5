87addf76f87ed38e5e7995b02444072be0db2198
Skarader <lukasholmwolf@gmail.com>
2024-03-22 18:41:24 +0100

    about-us done for review

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,23 +4,24 @@ export default function AboutUs() {
       <div className="bg-custom-green about-us-container">
         <h2 className="text-custom-white about-us-title">ABOUT US</h2>
         <p className="text-custom-white border-solid border-2 border-custom-yellow about-us-text">
-          Välkommen till E-Hammer, din ultimata destination för spännande
-          auktioner på nätet! Vi är stolta över att vara pionjärer inom den
-          elektroniska auktionsvärlden, och sedan vår grundande år 2024 har vi
-          skapat en plattform där djurälskare och samlare kan komma samman för
-          att upptäcka unika och exotiska djur. Vi på E-Hammer strävar efter att
-          erbjuda en säker och pålitlig miljö där våra användare kan köpa och
-          sälja djur av hög kvalitet. Med vår användarvänliga webbplats och
-          intuitiva gränssnitt är det enkelt att hitta det perfekta
-          sällskapsdjuret eller den ultimata tillökningen till din samling. Vår
-          passion för djur och vår strävan efter kundnöjdhet är kärnan i allt vi
-          gör. Vi arbetar nära våra säljare för att säkerställa att varje
-          auktionsobjekt beskrivs noggrant och att våra köpare får en fantastisk
-          upplevelse varje gång de handlar hos oss. På E-Hammer finns det något
-          för alla djurälskare, oavsett om du letar efter exotiska reptiler,
-          vackra fåglar, underbara sällskapsdjur eller något annat spännande.
-          Med ett brett utbud av arter och auktionsformat kan du vara säker på
-          att hitta det perfekta tillskottet till din familj eller samling.
+          <h4 className="mb-5 text-2xl">Välkommen till E-Hammer!</h4>
+          Din ultimata destination för spännande auktioner på nätet! Vi är
+          stolta över att vara pionjärer inom den elektroniska auktionsvärlden,
+          och sedan vår grundande år 2024 har vi skapat en plattform där
+          djurälskare och samlare kan komma samman för att upptäcka unika och
+          exotiska djur. Vi på E-Hammer strävar efter att erbjuda en säker och
+          pålitlig miljö där våra användare kan köpa och sälja djur av hög
+          kvalitet. Med vår användarvänliga webbplats och intuitiva gränssnitt
+          är det enkelt att hitta det perfekta sällskapsdjuret eller den
+          ultimata tillökningen till din samling. Vår passion för djur och vår
+          strävan efter kundnöjdhet är kärnan i allt vi gör. Vi arbetar nära
+          våra säljare för att säkerställa att varje auktionsobjekt beskrivs
+          noggrant och att våra köpare får en fantastisk upplevelse varje gång
+          de handlar hos oss. På E-Hammer finns det något för alla djurälskare,
+          oavsett om du letar efter exotiska reptiler, vackra fåglar, underbara
+          sällskapsdjur eller något annat spännande. Med ett brett utbud av
+          arter och auktionsformat kan du vara säker på att hitta det perfekta
+          tillskottet till din familj eller samling.
         </p>
       </div>
     </>
