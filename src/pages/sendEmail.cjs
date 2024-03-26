98bacbccf48b8922c7b619b5f91a3ade2312c721
Skarader <lukasholmwolf@gmail.com>
2024-03-26 13:51:22 +0100

    sendEmail test file

---
+++ src/pages/sendEmail.cjs
@@ -0,0 +1,34 @@
+// Import nodemailer module
+const nodemailer = require("nodemailer");
+
+const myEmailAdress = "myEmail@gmail.com";
+const myPassword = "XXX";
+
+// Create a transporter object using SMTP transport
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: `${myEmailAdress}`,
+    pass: `${myPassword}`,
+  },
+});
+
+// Email content
+let mailOptions = {
+  from: `${myEmailAdress}`,
+  to: "lukasholmwolf@gmail.com",
+  subject: "Test Email",
+  text: "This is a test email sent from Node.js using nodemailer.",
+};
+
+// Send email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log("Error occurred:", error);
+  } else {
+    console.log("Email sent:", info.response);
+  }
+});
+
+// npm install nodemailer
+// node sendEmail.js
