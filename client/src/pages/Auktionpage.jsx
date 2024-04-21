c58fdb451474eef039911b6f0fa9233bae8e8636
EddieMollerstrom <eddiemollerstrom123@gmail.com>
2024-04-21 15:58:59 +0200

    added checkboxes for the products tags

--- client/src/pages/Auktionpage.jsx
+++ client/src/pages/Auktionpage.jsx
@@ -6,8 +6,19 @@ function AuctionForm() {
   const [ends, setEndDate] = useState("");
   const [minimumBid, setminimumBid] = useState("");
   const [price, setprice] = useState("");
-  const [category, setCategory] = useState("");
   const [img, setImageURL] = useState("");
+
+  const [däggdjur, setDäggdjur] = useState(false);
+  const [kräldjur, setKräldjur] = useState(false);
+  const [fågel, setFågel] = useState(false);
+  const [rovdjur, setRovdjur] = useState(false);
+
+  const tags = [
+    däggdjur ? "Däggdjur" : "",
+    kräldjur ? "Kräldjur" : "",
+    fågel ? "Fågel" : "",
+    rovdjur ? "Rovdjur" : "",
+  ].filter((tag) => tag !== "");
 
   const formData = {
     title,
@@ -15,8 +26,8 @@ function AuctionForm() {
     ends,
     minimumBid,
     price,
-    category,
     img,
+    tags,
   };
 
   const handleSubmit = async (event) => {
@@ -24,46 +35,32 @@ function AuctionForm() {
       const response = await fetch(`/api/products`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
       if (response.ok) {
         const result = await response.json();
-        
         console.log(result);
-        window.location.reload();
       } else {
-        console.log('statuskod:', response);
+        console.log("statuskod:", response);
       }
     } catch (error) {
       console.error("Något gick fel:", error);
     }
 
-
     event.preventDefault();
-  };
 
-  const animalCategories = [
-    { category: "Horse", tags: "horse" },
-    { category: "Cat", tags: "cat" },
-    { category: "Dog", tags: "dog" },
-    { category: "Others", tags: "others" }
-  ];
-
-  const handleCheckboxChange = (category) => {
-    // Toggle category selection
-    if (categories.includes(category)) {
-      setCategories(categories.filter((cat) => cat !== category));
-    } else {
-      setCategories([...categories, category]);
-    }
+    console.log("Formulärdata:", formData);
   };
 
   return (
     <div className="flex justify-center min-h-screen p-4">
       <div className="w-8/12">
         <h2 className="text-2xl font-bold text-center mb-4">Skapa annons</h2>
-        <form className="bg-custom-grey rounded flex flex-col p-7 gap-3 text-custom-white annonscontainer" encType="multipart/form-data">
+        <form
+          className="bg-custom-grey rounded flex flex-col p-7 gap-3 text-custom-white annonscontainer"
+          encType="multipart/form-data"
+        >
           <label htmlFor="title">Titel:</label>
           <input
             type="text"
@@ -73,7 +70,6 @@ function AuctionForm() {
             value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
-
 
           <label htmlFor="description">Beskrivning:</label>
           <textarea
@@ -86,8 +82,6 @@ function AuctionForm() {
           ></textarea>
 
           <label htmlFor="ends">Slutdatum för Auktion:</label>
-
-          <label htmlFor="endDate">Slutdatum för Auktion:</label>
           <input
             type="datetime-local"
             id="ends"
@@ -117,33 +111,64 @@ function AuctionForm() {
             onChange={(event) => setprice(event.target.value)}
           />
 
-          <label>Kategori/Sökord:</label>
-          <div>
-            {animalCategories.map((animal, index) => (
-              <div key={index}>
-                <label htmlFor={animal.tag}>{animal.category}</label>
-                <input
-                  type="checkbox"
-                  id={animal.tag}
-                  name="category"
-                  value={animal.category}
-                  checked={categories.includes(animal.category)}
-                  onChange={() => handleCheckboxChange(animal.category)}
-                />
-              </div>
-            ))}
-          </div>
+          <section className="flex gap-5">
+            <div className="flex gap-1">
+              <label htmlFor="däggdjur">Däggdjur</label>
+              <input
+                id="däggdjur"
+                type="checkbox"
+                checked={däggdjur}
+                value="däggdjur"
+                onChange={() => setDäggdjur(true)}
+              />
+            </div>
 
+            <div className="flex gap-1">
+              <label htmlFor="kräldjur">Kräldjur</label>
+              <input
+                type="checkbox"
+                checked={kräldjur}
+                id="kräldjur"
+                onChange={() => setKräldjur(true)}
+              />
+            </div>
+
+            <div className="flex gap-1">
+              <label htmlFor="fågel">Fågel</label>
+              <input
+                type="checkbox"
+                checked={fågel}
+                id="fågel"
+                onChange={() => setFågel(true)}
+              />
+            </div>
+
+            <div className="flex gap-1">
+              <label htmlFor="rovdjur">Rovdjur</label>
+              <input
+                type="checkbox"
+                checked={rovdjur}
+                id="rovdjur"
+                onChange={() => setRovdjur(true)}
+              />
+            </div>
+          </section>
+
+          <label
+            htmlFor="imageURL"
+            className="bg-custom-yellow h-8 w-60 flex justify-center items-center rounded-full text-custom-green cursor-pointer"
+          >
+            Bild URL
+          </label>
           <input
             type="text"
             id="img"
             name="img"
-            className="text-black, p-2"
+            className="text-black"
             placeholder="Ange bildens URL"
             value={img}
             onChange={(event) => setImageURL(event.target.value)}
           />
-
 
           <input
             type="button"
