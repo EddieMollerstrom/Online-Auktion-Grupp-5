cb818935642f8b2b0698165ed1a54d62ac181393
Skarader <lukasholmwolf@gmail.com>
2024-04-28 23:59:39 +0200

    saced changes in routes

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -39,7 +39,7 @@ const Router = createBrowserRouter(
             path="/MyPages/SavedObjects"
             element={<MyPagesSavedObjects />}
           />
-          <Route path="/Auktionpage"></Route>
+          <Route path="/MyPages/Auktionpage"></Route>
           <Route path="/MyPages/SoldObjects" element={<MyPagesSold />} />
         </Route>
       </Route>
