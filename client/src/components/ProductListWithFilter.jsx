c6ed14c568ebf39536124a6d4718ec53f04af12e
Skarader <lukasholmwolf@gmail.com>
2024-04-30 22:16:46 +0200

    trying to implement name and price filtering

--- client/src/components/ProductListWithFilter.jsx
+++ client/src/components/ProductListWithFilter.jsx
@@ -3,17 +3,9 @@ import ProductCard from "./ProductCard";
 
 export default function ProductListWithFilter({ products }) {
   const [searchInput, setSearchInput] = useState("");
-  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [sortOrder, setSortOrder] = useState("ascending");
-
-  const handleOutsideClick = (event) => {
-    if (!event.target.closest(".SearchContainer")) {
-      setIsDropdownVisible(false);
-    }
-  };
-
-  document.addEventListener("mousedown", handleOutsideClick);
+  const [sortOrderName, setSortOrderName] = useState("ascendingName");
 
   const filteredProducts = products
     ? products.filter((product) => {
@@ -30,30 +22,44 @@ export default function ProductListWithFilter({ products }) {
       })
     : [];
 
-  const toggleSortOrder = () => {
-    setSortOrder((prevSortOrder) =>
-      prevSortOrder === "ascending" ? "descending" : "ascending"
-    );
-  };
-
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-    setIsDropdownVisible(false);
-  };
-
   const filteredByCategory = selectedCategory
     ? filteredProducts.filter((product) =>
         product.category.toLowerCase().includes(selectedCategory.toLowerCase())
       )
     : filteredProducts;
 
-  const sortedData = filteredByCategory.sort((a, b) => {
+  const toggleSortOrder = () => {
+    setSortOrder((prevSortOrder) =>
+      prevSortOrder === "ascending" ? "descending" : "ascending"
+    );
+  };
+
+  const toggleSortOrderName = () => {
+    setSortOrderName((prevSortOrderName) =>
+      prevSortOrderName === "ascendingName" ? "descendingName" : "ascendingName"
+    );
+  };
+
+  const sortedDataByPrice = filteredByCategory.sort((a, b) => {
     if (sortOrder === "ascending") {
       return a.price - b.price;
     } else {
       return b.price - a.price;
     }
   });
+
+  const sortedDataByName = filteredByCategory.sort((a, b) => {
+    const nameA = a.title.toLowerCase();
+    const nameB = b.title.toLowerCase();
+    if (sortOrderName === "ascendingName") {
+      return nameA.localeCompare(nameB);
+    } else {
+      return nameB.localeCompare(nameA);
+    }
+  });
+
+  const sortedDataToDisplay =
+    sortOrderName === "ascendingName" ? sortedDataByName : sortedDataByPrice;
 
   return (
     <>
@@ -74,23 +80,6 @@ export default function ProductListWithFilter({ products }) {
                   setSearchInput(e.target.value);
                 }}
               />
-              {/*   <button
-                className="button"
-                onClick={() => setIsDropdownVisible(!isDropdownVisible)}
-              >
-                Start
-              </button>
-              <div className={`dropdown ${isDropdownVisible ? "active" : ""}`}>
-                {products.map((item) => (
-                  <div
-                    key={item.id}
-                    className="dropdown-row"
-                    onClick={() => handleCategoryClick(item.category)}
-                  >
-                    {item.category}
-                  </div>
-                ))}
-              </div> */}
             </div>
           </div>
         </div>
@@ -103,7 +92,7 @@ export default function ProductListWithFilter({ products }) {
             </div>
             <div className="flex w-9/12 items-start pl-10 gap-1 text-xl font-medium">
               <button
-                className="relative transition-all duration-500 hover:text-custom-green"
+                className="relative transition-all duration-500 hover:text-custom-green mr-12"
                 onClick={toggleSortOrder}
                 style={{ overflow: "hidden" }}
               >
@@ -113,9 +102,20 @@ export default function ProductListWithFilter({ products }) {
                 ></span>
                 Pris
               </button>
+              <button
+                className="relative transition-all duration-500 hover:text-custom-green"
+                onClick={toggleSortOrderName}
+                style={{ overflow: "hidden" }}
+              >
+                <span
+                  className="absolute bottom-0 left-0 bg-custom-green h-0.5 w-0 transition-all duration-500 hover:w-full"
+                  style={{ transitionTimingFunction: "ease-out" }}
+                ></span>
+                Namn
+              </button>
             </div>
             <ul className="list-none grid grid-cols-4 gap-10">
-              {sortedData.map((product) => (
+              {sortedDataToDisplay.map((product) => (
                 <ProductCard key={product.id} product={product} />
               ))}
             </ul>
